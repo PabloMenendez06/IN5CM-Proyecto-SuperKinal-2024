5fdd35d709d9db307ea98a7dfff1942e3f7fcdaa
PabloMenendez06 <pmenedez-2020332@kinal.edu.gt>
2024-06-05 11:46:32 -0600

    Reporte-Facturas

--- src/org/pablomenendez/system/Main.java
+++ src/org/pablomenendez/system/Main.java
@@ -53,7 +53,7 @@ public class Main extends Application {
     public void start(Stage stage) throws Exception {
         this.stage = stage;
         stage.setTitle("Super Kinal Gragas App");
-        menuComprasView();
+        formRegistrarUsuarioView();
 
         stage.show();
 
